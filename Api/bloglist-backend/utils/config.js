f46f6d63ed11f5477ebf957d7eca40acd7f348b1
fporfirio <facundojporfirio@gmail.com>
2021-11-13 22:11:34 -0300

    fixed cors error in config file

--- Api/bloglist-backend/utils/config.js
+++ Api/bloglist-backend/utils/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const JWKSECRET = process.env.JWKSECRET;
 const CORS_OPTS = {
-  origin: "https://fporfirio.github.io/ReactBlog",
+  origin: "https://fporfirio.github.io",
 };
 let MONGODB_URI = process.env.MONGODB_URI;
 
