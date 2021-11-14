273f2b8c6651895556e17eb8fc7653124a0ded81
fporfirio <facundojporfirio@gmail.com>
2021-11-13 23:13:40 -0300

    fixed logical error on api config file

--- Api/bloglist-backend/utils/config.js
+++ Api/bloglist-backend/utils/config.js
@@ -5,9 +5,10 @@ const JWKSECRET = process.env.JWKSECRET;
 const CORS_OPTS = {
   origin: "https://fporfirio.github.io",
 };
-let MONGODB_URI = process.env.MONGODB_URI;
 
-if (process.env.NODE_ENV === "test" || "development") {
+let MONGODB_URI = process.env.MONGODB_URI;
+if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+  console.log("wasup");
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
